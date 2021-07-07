2b5c641c30926b5b25060875c652ca2c223146ec
andresb <andresb@LAPTOP-UM9V32OK>
2021-07-07 16:55:41 -0400

    Primer commit- configuracion de jwt, servicios de login, plantilla mud blazor

---
+++ PlannerApp/PlannerApp/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public class JwtAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public JwtAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (await _localStorage.ContainKeyAsync("access_token"))
+            {
+                //el usuario está logueado
+                var tokenAsString = await _localStorage.GetItemAsStringAsync("access_token");
+                var tokenHanlder = new JwtSecurityTokenHandler();
+
+                var token = tokenHanlder.ReadJwtToken(tokenAsString);
+                var identity = new ClaimsIdentity(token.Claims, "Bearer");
+                var user = new ClaimsPrincipal(identity);
+                var authState = new AuthenticationState(user);
+
+                //con esto se notifica a todos los demas componentes que se ingreso
+                NotifyAuthenticationStateChanged(Task.FromResult(authState));
+
+                return authState;
+            }
+
+            //quiere decir que el usuario no esta registrado
+            return new AuthenticationState(new ClaimsPrincipal());
+        }
+    }
+}
---
+++ PlannerApp/PlannerApp/Program.cs
@@ -0,0 +1,38 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddHttpClient("PlannerApp.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net/");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+            builder.Services.AddScoped(sp =>
+                sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddMudServices();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
